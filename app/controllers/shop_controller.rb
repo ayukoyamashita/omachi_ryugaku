a0c3a2f83a5dbcc93bc33dc36865c92e676aaa81
ayubntn <coccoboy_am@yahoo.co.jp>
2023-01-26 18:53:42 +0900

    routes、controller、view最低限

---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,18 @@
+class ShopController < ApplicationController
+
+	def index
+
+	end
+
+	def detail
+
+	end
+
+	def videos
+
+	end
+
+	def video_detail
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  root "top#index"
+  # Defines the root path route ('/')
+  root 'top#index'
+
+	get 'shops', to: 'shop#index'
+	get 'shops/:shop_id', to: 'shop#detail', as: 'shop_detail'
+	get 'shops/:shop_id/videos', to: 'shop#videos', as: 'shop_videos'
+  get 'shops/:shop_id/videos/:video_id', to: 'shop#video_detail', as: 'video_detail'
+
+	get 'videos', to: 'video#index'
+
 end
