cbbd8f54dc52e272e9aa8d6c168670f0ff1ec5d6
ayubntn <coccoboy_am@yahoo.co.jp>
2023-01-27 11:13:06 +0900

    お店一覧　データ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# https://womo.jp/shop/detail/309/
 Shop.create!(
 	name: "CHA CA（茶花）",
 	image_path: "shops/chaka/main.jpeg",
@@ -19,6 +20,7 @@ Shop.create!(
 	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
 )
 
+# https://womo.jp/shop/detail/827/
 Shop.create!(
 	name: "すろ～カフェ モミの木",
 	image_path: "shops/momi/main.jpeg",
@@ -38,4 +40,136 @@ Shop.create!(
 	learn_day1: '一日目の内容です一日目の内容です一日目の内容です一日目の内容です一日目の内容です',
 	learn_day2: '二日目の内容です',
 	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
-	)
+)
+
+# https://womo.jp/shop/detail/2951/
+Shop.create!(
+	name: "Loulou et Minette",
+	image_path: "shops/ruruemi/main.jpeg",
+	catch: "静岡の料理コンクールで優勝した実力店",
+	owner_name: 'オーナーの名前',
+	owner_image_path: 'owner_sample.jpg',
+	owner_profile: 'オーナーのプロフィール',
+	description: "県立美術館近くにある洋食と手づくりスイーツのお店。ここではフレンチシェフがつくる本格的なサンドイッチが味わえる。有名ホテルの製パン課や厨房を経験したオーナーがパン生地からこだわり焼き上げる。その実力は静岡の料理コンクールで優勝したほど。自家製食パンも販売中（1斤390円）。",
+	description_image: "/shops/ruruemi/desc_image.jpeg",
+	address: "422-8002 静岡県静岡市駿河区谷田33-30",
+	learn_image1: 'dummy.png',
+	learn_image2: 'dummy.png',
+	learn_image3: 'dummy.png',
+	learn_point1: '学ぶポイント１',
+	learn_point2: '学ぶポイント２',
+	learn_point3: '学ぶポイント３',
+	learn_day1: '一日目の内容です一日目の内容です一日目の内容です一日目の内容です一日目の内容です',
+	learn_day2: '二日目の内容です',
+	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
+)
+
+# https://womo.jp/shop/detail/3713/
+Shop.create!(
+	name: "811 privategym",
+	image_path: "shops/811/main.jpeg",
+	catch: "日常に馴染む無理のないトレーニング法 完全プライベート空間のパーソナルジム",
+	owner_name: 'オーナーの名前',
+	owner_image_path: 'owner_sample.jpg',
+	owner_profile: 'オーナーのプロフィール',
+	description: "細いだけの身体ではなく、バランスが取れたメリハリのあるボディメイク法を指導するプライベートジム。月会費制の大型ジムでは満足のいく結果が出せなかった人、高価なパーソナルジムでは継続できない人、なんとなく運動を始めてみたいけど何をしたら良いかわからない人、食事制限のダイエットはしたくない人など、幅広い悩みに対応。女性トレーナーがマンツーマンで指導してくれるから、運動が苦手な方でも無理なく続けられる。まずはトライアルトレーニングから試してみよう。",
+	description_image: "/shops/811/desc_image.jpeg",
+	address: "420-0839 静岡県静岡市葵区鷹匠2丁目21-8日吉さかえビル2F",
+	learn_image1: 'dummy.png',
+	learn_image2: 'dummy.png',
+	learn_image3: 'dummy.png',
+	learn_point1: '学ぶポイント１',
+	learn_point2: '学ぶポイント２',
+	learn_point3: '学ぶポイント３',
+	learn_day1: '一日目の内容です一日目の内容です一日目の内容です一日目の内容です一日目の内容です',
+	learn_day2: '二日目の内容です',
+	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
+)
+
+# https://womo.jp/shop/detail/2957/
+Shop.create!(
+	name: "MILK FLOWERS",
+	image_path: "shops/milk/main.jpeg",
+	catch: "スタイリッシュで落ち着いた、大人な雰囲気。日常に添える花と緑で心地よい暮らしを",
+	owner_name: 'オーナーの名前',
+	owner_image_path: 'owner_sample.jpg',
+	owner_profile: 'オーナーのプロフィール',
+	description: "モノトーンを基調としたおしゃれな店内には、植物や器がまるで美術館のように美しくディスプレイされている。生花のほか、ドライフラワー、花器、鉢なども豊富にそろう。大きな観葉植物の取り扱いも多く、インテリアグリーンの提案に長けている。育て方のアドバイスやアフターケアにも丁寧に対応してくれるため、気軽に相談ができる。デザイン性の高いアレンジメントが好評で、ちょっとしたフラワーギフトからイベントのディスプレイまで。さらにウエディングはブーケ、トータルコーディネートが可能。",
+	description_image: "/shops/milk/desc_image.jpeg",
+	address: "420-0064 静岡県静岡市葵区本通8-2-10",
+	learn_image1: 'dummy.png',
+	learn_image2: 'dummy.png',
+	learn_image3: 'dummy.png',
+	learn_point1: '学ぶポイント１',
+	learn_point2: '学ぶポイント２',
+	learn_point3: '学ぶポイント３',
+	learn_day1: '一日目の内容です一日目の内容です一日目の内容です一日目の内容です一日目の内容です',
+	learn_day2: '二日目の内容です',
+	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
+)
+
+# https://womo.jp/shop/detail/2958/
+Shop.create!(
+	name: "SNUGGERY flowers",
+	image_path: "shops/snug/main.jpeg",
+	catch: "日常を飾る一輪から特別な日の装飾まで、さまざまなシーンを彩る",
+	owner_name: 'オーナーの名前',
+	owner_image_path: 'owner_sample.jpg',
+	owner_profile: 'オーナーのプロフィール',
+	description: "シックで落ち着きのある、大人向けの花屋さん。行事や季節の花など、普段の生活に取り入れやすい花を多く取り扱っている。自宅用の切り花のほか、ドライフラワーや鉢植えもおすすめ。花を飾る花瓶やカゴも扱っているので、お部屋に合わせた飾り方を選ぼう。ブライダルはブーケからトータルコーディネートまで。ほかには店舗ディスプレイ、花束・スタンドなどのフラワーギフトもお好み通り。一つひとつのお花の個性に合わせたアレンジで'想い'をかたちにしてくれる。オーダーはウェブからでもOK、配達も対応している。",
+	description_image: "/shops/snug/desc_image.jpeg",
+	address: "420-0064 静岡県静岡市葵区本通8-2-10",
+	learn_image1: 'dummy.png',
+	learn_image2: 'dummy.png',
+	learn_image3: 'dummy.png',
+	learn_point1: '学ぶポイント１',
+	learn_point2: '学ぶポイント２',
+	learn_point3: '学ぶポイント３',
+	learn_day1: '一日目の内容です一日目の内容です一日目の内容です一日目の内容です一日目の内容です',
+	learn_day2: '二日目の内容です',
+	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
+)
+
+# https://womo.jp/shop/detail/364/
+Shop.create!(
+	name: "和CAFE 茶楽",
+	image_path: "shops/charaku/main.jpeg",
+	catch: "古民家風のカフェで、お茶の魅力再発見",
+	owner_name: 'オーナーの名前',
+	owner_image_path: 'owner_sample.jpg',
+	owner_profile: 'オーナーのプロフィール',
+	description: "旧東海道の名残がある興津の旧国道一沿い。レトロな雰囲気が漂う和カフェは、この地で1925年創業の茶問屋・山梨商店が開いた店。店内は、漆喰の壁や天竜杉など自然素材を使った居心地のいい空間。丁寧に作られるランチは、心温まる優しい味。駿河湾の特産 桜えびの甘さが引き立つ、ほんのりピンク色に染まるご飯のお供は、同じく地元で人気の、興津「出口魚店」の名物黒はんぺん。食後には、産地ごとの味の違いが楽しめる静岡茶や、大人好みの和のスイーツやお茶屋ならでは抹茶スイーツが堪能できる。",
+	description_image: "/shops/charaku/desc_image.jpeg",
+	address: "424-0205 静岡県静岡市清水区興津本町158-1",
+	learn_image1: 'dummy.png',
+	learn_image2: 'dummy.png',
+	learn_image3: 'dummy.png',
+	learn_point1: '学ぶポイント１',
+	learn_point2: '学ぶポイント２',
+	learn_point3: '学ぶポイント３',
+	learn_day1: '一日目の内容です一日目の内容です一日目の内容です一日目の内容です一日目の内容です',
+	learn_day2: '二日目の内容です',
+	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
+)
+
+# https://womo.jp/shop/detail/3425/
+Shop.create!(
+	name: "ファイブフィートカフェ（five feet cafe's）",
+	image_path: "shops/five/main.jpeg",
+	catch: "風が気持ちいいテラス席でごちそうバーガーをいただこう！ノンアルコールカクテル片手にママ会＆女子会はいかが",
+	owner_name: 'オーナーの名前',
+	owner_image_path: 'owner_sample.jpg',
+	owner_profile: 'オーナーのプロフィール',
+	description: "ボリューミーなハンバーガーが食べられると話題の、清水の人気カフェがリニューアルOPEN。こちらのバーガーは国産小麦と天然酵母を使ったパンが評判の「PLAIN BAKERY」のバンズを使用。表面はカリッと香ばしく、中はふんわりとしていて具材を引き立てる。4種類のバーガーから選べるお得なランチセットでぜひお試しを。またドリンクメニューも充実。果物や野菜を使ったヘルシードリンクやノンアルコールカクテルが種類豊富に揃う。南国風の店内には、ゆっくり過ごせるソファ席と開放的なテラス席をご用意。こだわりバンズのバーガーと好みのドリンクで、お昼から優雅なひとときを過ごしてみては。駐車場が広く子ども連れも歓迎なので、ママ友同士の集まりにもおすすめ。注文はカウンターでするセルフ形式なので、気軽に立ち寄れる。料理のテイクアウトや「PLAIN BAKERY」のパンやスイーツの販売も好評。",
+	description_image: "/shops/five/desc_image.jpeg",
+	address: "424-0044 静岡県静岡市清水区江尻台町22-35",
+	learn_image1: 'dummy.png',
+	learn_image2: 'dummy.png',
+	learn_image3: 'dummy.png',
+	learn_point1: '学ぶポイント１',
+	learn_point2: '学ぶポイント２',
+	learn_point3: '学ぶポイント３',
+	learn_day1: '一日目の内容です一日目の内容です一日目の内容です一日目の内容です一日目の内容です',
+	learn_day2: '二日目の内容です',
+	learn_day3: '三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です三日目の内容です',
+)
