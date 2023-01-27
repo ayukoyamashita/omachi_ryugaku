786ef54b3257f26ec79da130ef49a87e8623dcfa
ayubntn <coccoboy_am@yahoo.co.jp>
2023-01-27 15:13:47 +0900

    動画サムネなど

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_27_020552) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_055109) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_27_020552) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "shop_id", comment: "店舗ID"
+    t.text "description"
+    t.string "thumb_path"
+    t.string "thumb_mini_path"
   end
 
 end
